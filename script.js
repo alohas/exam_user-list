63bfcca8826fc17e881621a143e641f14de20b24
Dominykas Kutka <dominykaskulka@yahoo.com>
2019-12-01 22:35:41 +0100

    putting data

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
 const dbLink = "https://rpsexam-61a3.restdb.io/rest/registeredusers";
 const key = "5ddfb3cc4658275ac9dc201e";
+let tabledata = [];
 
-window.addEventListener("DOMContentLoaded", event => {
-  get();
+var table = new Tabulator("#user-list", {
+  cellEdited: function(cell) {
+    console.log(cell);
+    put(cell._cell.row.data, cell._cell.row.data._id);
+  },
+  dataEdited: function(data) {},
+  layout: "fitColumns", //fit columns to width of table (optional)
+  columns: [
+    //Define Table Columns
+    {
+      title: "Username",
+      field: "username",
+      validator: ["unique", "required"],
+      editor: "input",
+      width: 150
+    },
+    {
+      title: "Password",
+      field: "password",
+      validator: "required",
+      editor: "input",
+      align: "left"
+    },
+    {
+      title: "Coins",
+      field: "coins",
+      validator: ["required", "integer"],
+      editor: "number",
+      editorParams: {
+        verticalNavigation: "editor",
+        min: 0,
+        max: 100000000000000000,
+        step: 100
+      }
+    },
+    {
+      title: "Email",
+      field: "email",
+      validator: ["unique", "required", "regex:\\@", "string"],
+      editor: "input",
+      editorParams: {
+        search: true,
+        elementAttributes: {
+          maxlength: "10" //set the maximum character length of the input element to 10 characters
+        }
+      }
+    }
+  ]
 });
 
 function get() {
@@ -15,16 +62,39 @@ function get() {
     }
   })
     .then(e => e.json())
-    .then(data => display(data));
+    .then(data => {
+      display(data);
+      console.log(data);
+    });
 }
 
 function display(users) {
   users.forEach(user => {
     //console.log(user);
-    let clone = document.querySelector(".listing").content.cloneNode(true);
-    clone.querySelector(".username").textContent = user.username;
-    clone.querySelector(".password").textContent = user.password;
-    clone.querySelector(".email").textContent = user.email;
-    document.querySelector(".user-list").appendChild(clone);
+    tabledata.push(user);
   });
+  //console.log(tabledata);
+  table.setData(tabledata);
 }
+
+function put(data, id) {
+  console.log(data);
+
+  const json = JSON.stringify(data);
+  console.log(json);
+  fetch(`${dbLink}/${id}`, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": key,
+      "cache-control": "no-cache"
+    },
+    body: json
+  })
+    .then(d => d.json())
+    .then(t => console.log(t));
+}
+
+window.addEventListener("DOMContentLoaded", event => {
+  get();
+});
